22f612e7417651a9fd10e8adaf8a80f67970c474
Larissa Silvério <desbessel.larissa@gmail.com>
2026-01-08 00:10:49 -0300

    creating Application Layer for use cases - response

---
+++ demo/src/main/application/dto/response/AuthResponse.java
@@ -0,0 +1,24 @@
+package com.sghss.application.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String token;
+    private String type = "Bearer";
+    private String email;
+    private String name;
+    private String role;
+
+    public AuthResponse(String token, String email, String name, String role) {
+        this.token = token;
+        this.email = email;
+        this.name = name;
+        this.role = role;
+    }
+}
